486eef8cfc5ef470ad18535dbbbe97d464560e53
gitzak <zakaria.belrhali@gmail.com>
2024-10-31 16:18:40 +0100

    RateLimiter

---
+++ app/Providers/RateLimitServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+
+class RateLimitServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('api', fn () => Limit::perMinute(60));
+
+        RateLimiter::for('auth', fn () => Limit::perMinute(5));
+    }
+}
